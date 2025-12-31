a03fa7e5da19b448d5c0c0d5a0efc658202d77e6
Mark Menchavez <markchristian.menchavez@gmail.com>
2025-12-31 16:01:17 +0800

    Playground.Api WeatherForecasts

---
+++ src/Api/Playground.Api/WeatherForecast.cs
@@ -0,0 +1,31 @@
+namespace Playground.Api;
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+internal static class EndpointRouteBuilderExtensions
+{
+    public static IEndpointRouteBuilder MapWeatherForecast(this IEndpointRouteBuilder app)
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", () =>
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast(
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]))
+                .ToArray();
+            return forecast;
+        })
+        .WithName("GetWeatherForecast");
+
+        return app;
+    }
+}
