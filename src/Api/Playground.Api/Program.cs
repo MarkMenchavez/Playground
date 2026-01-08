25361038ddfba86aefa95cfa54ce3929a33efcd0
Mark Menchavez <markchristian.menchavez@gmail.com>
2026-01-08 19:02:24 +0800

    Make WeatherForecast configurable and cancellable

--- src/Api/Playground.Api/Program.cs
+++ src/Api/Playground.Api/Program.cs
@@ -2,9 +2,13 @@ using Playground.Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddWeatherForecast(builder.Configuration);
 
 var app = builder.Build();
 
@@ -14,7 +18,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-////app.UseHttpsRedirection();
+app.UseHsts();
+app.UseHttpsRedirection();
 
 app.MapWeatherForecast();
 
--- src/Api/Playground.Api/WeatherForecast.cs
+++ src/Api/Playground.Api/WeatherForecast.cs
@@ -1,31 +1,132 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
 namespace Playground.Api;
+
+internal interface IWeatherForecastService
+{
+    Task<IEnumerable<WeatherForecast>> GetForecastsAsync(CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<WeatherForecast>> GetForecastsAsync(int days, CancellationToken cancellationToken = default);
+}
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
 
+internal static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddWeatherForecast(this IServiceCollection services, Action<WeatherForecastServiceOptions> configureOptions)
+    {
+        services.AddOptions<WeatherForecastServiceOptions>()
+            .Configure(configureOptions);
+
+        services.AddWeatherForecast();
+
+        return services;
+    }
+
+    public static IServiceCollection AddWeatherForecast(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions<WeatherForecastServiceOptions>()
+            .Bind(configuration.GetSection(nameof(WeatherForecastService)));
+
+        services.AddWeatherForecast();
+
+        return services;
+    }
+
+    private static IServiceCollection AddWeatherForecast(this IServiceCollection services)
+    {
+        services.TryAddTransient<IWeatherForecastService, WeatherForecastService>();
+        services.ConfigureWeatherForecastServiceOptions();
+
+        return services;
+    }
+
+    private static OptionsBuilder<WeatherForecastServiceOptions> ConfigureWeatherForecastServiceOptions(this IServiceCollection services)
+    {
+        return services.AddOptions<WeatherForecastServiceOptions>()
+             .Validate(o => o.DefaultDays > 0, "DefaultDays must be greater than zero.")
+             .Validate(o => o.Summaries.Length > 0, "Summaries must contain at least one summary.")
+             .Validate(o => o.GenerationDelayMilliseconds >= 0, "GenerationDelayMilliseconds must be zero or a positive integer.")
+             .Validate(o => o.MinimumTemperatureCelsius < o.MaximumTemperatureCelsius, "MinimumTemperatureCelsius must be less than MaximumTemperatureCelsius.")
+             .ValidateOnStart();
+    }
+}
+
 internal static class EndpointRouteBuilderExtensions
 {
     public static IEndpointRouteBuilder MapWeatherForecast(this IEndpointRouteBuilder app)
     {
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        app.MapGet("/weatherforecast", async (IWeatherForecastService service, CancellationToken cancellationToken)
+                => TypedResults.Ok(await service.GetForecastsAsync(cancellationToken)))
+            .WithName("GetWeatherForecast")
+            .AddOpenApiOperationTransformer((operation, context, cancelalationToken) =>
+            {
+                operation.Summary = "Gets the weather forecast for the default number of days.";
+                operation.Description = "Retrieves an array of weather forecast data for the default number of days configured in the service.";
 
-        app.MapGet("/weatherforecast", () =>
-        {
-            var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast(
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]))
-                .ToArray();
-            return forecast;
-        })
-        .WithName("GetWeatherForecast");
+                return Task.CompletedTask;
+            });
 
         return app;
+    }
+}
+
+internal static partial class WeatherForecastLogger
+{
+    [LoggerMessage(
+        Level = LogLevel.Debug,
+        Message = "{Count} weather forecast generated.")]
+    public static partial void WeatherForecastGenerated(
+        this ILogger<WeatherForecastService> logger,
+        int count);
+}
+
+internal class WeatherForecastServiceOptions
+{
+    public int DefaultDays { get; set; }
+
+    public string[] Summaries { get; set; } = [];
+
+    public int GenerationDelayMilliseconds { get; set; } = 10;
+
+    public int MinimumTemperatureCelsius { get; set; } = -20;
+
+    public int MaximumTemperatureCelsius { get; set; } = 55;
+}
+
+internal class WeatherForecastService(
+    IOptions<WeatherForecastServiceOptions> options,
+    ILogger<WeatherForecastService> logger)
+    : IWeatherForecastService
+{
+    private readonly WeatherForecastServiceOptions serviceOptions = options.Value;
+
+    public Task<IEnumerable<WeatherForecast>> GetForecastsAsync(CancellationToken cancellationToken = default)
+    {
+        return GetForecastsAsync(serviceOptions.DefaultDays, cancellationToken);
+    }
+
+    public async Task<IEnumerable<WeatherForecast>> GetForecastsAsync(int days, CancellationToken cancellationToken = default)
+    {
+        var summaries = serviceOptions.Summaries;
+
+        var forecasts = new List<WeatherForecast>();
+
+        foreach (var index in Enumerable.Range(1, days))
+        {
+            await Task.Delay(serviceOptions.GenerationDelayMilliseconds, cancellationToken);
+            forecasts.Add(new WeatherForecast(
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index - 1)),
+                Random.Shared.Next(serviceOptions.MinimumTemperatureCelsius, serviceOptions.MaximumTemperatureCelsius),
+                summaries[Random.Shared.Next(summaries.Length)]));
+        }
+
+        logger.WeatherForecastGenerated(forecasts.Count);
+
+        return forecasts;
     }
 }
