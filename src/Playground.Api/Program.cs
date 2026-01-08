8cd64acae8aa26436b9943baa28bfe5b0265eba5
Mark Menchavez <markchristian.menchavez@gmail.com>
2026-01-08 19:45:11 +0800

    Use Centralized Packages Use Serilog

---
+++ src/Playground.Api/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Playground.Api;
+using Playground.Infrastructure;
+using Serilog;
+
+Log.Logger = SerilogExtensions.CreateBootstrapLogger();
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Logging.ClearProviders();
+    builder.Logging.AddConsole();
+
+    // Add services to the container.
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+    builder.Services.AddWeatherForecast(builder.Configuration);
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseHsts();
+    app.UseHttpsRedirection();
+
+    app.MapWeatherForecast();
+
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly.");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
